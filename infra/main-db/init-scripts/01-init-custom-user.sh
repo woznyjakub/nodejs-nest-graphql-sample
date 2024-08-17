f37ebbcebc82bd9f6bb574c9a3a5eb11e3377cdd
woznyjakub <jakub_wozny_01@wp.pl>
2024-08-17 15:58:14 +0200

    add database configuration (non-js level)

---
+++ infra/main-db/init-scripts/01-init-custom-user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+# create non-root user
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-EOSQL
+    CREATE USER "$APP_USER" WITH PASSWORD '$APP_USER_PASSWORD';
+    GRANT CONNECT ON DATABASE "$POSTGRES_DB" TO "$APP_USER";
+    GRANT USAGE ON SCHEMA public TO "$APP_USER";
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "$APP_USER";
+EOSQL
+
+echo "User $APP_USER created."
